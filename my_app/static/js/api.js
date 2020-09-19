294afb6d68f7530816fc73b0955a1c8b29ce7f83
Jason G <jasongasior@live.com>
2020-09-19 14:11:48 -0500

    changed stuff around

--- my_app/static/js/api.js
+++ my_app/static/js/api.js
@@ -1,16 +1,10 @@
-function getCat() {
-    fetch("https://api.thecatapi.com/v1/images/search?api_key=a7ed07bb-660b-4184-8084-4163dfd5bc14")
-    .then(res => res.json())
-    .then(res=> {
-        const div = document.getElementById("cat-div");
-        div.innerHTML = '';
-        const cat = new Image();
-        cat.src = res[0].url;
-        div.appendChild(cat);
-    });
-}
-
-window.onload = function() {
-    const catButton = document.getElementById("cat-button");
-    catButton.onclick = getCat;
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
 }
