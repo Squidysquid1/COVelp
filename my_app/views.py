c8bd6a74c078c24026d4c6b615caacf90f6ff4f0
StanleyN1 <stan.nicholson1@gmail.com>
2020-09-19 13:00:31 -0500

    Commented out db code

--- my_app/views.py
+++ my_app/views.py
@@ -18,30 +18,30 @@ def index():
 
     return render_template("index.html", name=name, facts=fact_dict, posts=post_list)
 
-@app.route("/change_name")
-def change_name():
-    global name
-    new_name = request.args.get('name')
-    name = new_name
-    return redirect("/")
+# @app.route("/change_name")
+# def change_name():
+#     global name
+#     new_name = request.args.get('name')
+#     name = new_name
+#     return redirect("/")
+#
+# @app.route("/post", methods=["POST"])
+# def post():
+#     if request.method == "POST":
+#         print(request)
+#         post_info = request.get_json()
+#         new_post = Post(title=post_info['title'], description=post_info['description'])
+#         db.session.add(new_post)
+#         db.session.commit()
+#    return redirect("/")
 
-@app.route("/post", methods=["POST"])
-def post():
-    if request.method == "POST":
-        print(request)
-        post_info = request.get_json()
-        new_post = Post(title=post_info['title'], description=post_info['description'])
-        db.session.add(new_post)
-        db.session.commit()
-    return redirect("/")
-
-@app.route("/change_facts", methods=["POST"])
-def change_facts():
-    if request.method == "POST":
-        change_facts = request.get_json()
-        for key, value in change_facts.items():
-            if Fact.query.filter(Fact.name == key).first() is None:
-                new_fact = Fact(name=key, value=value)
-                db.session.add(new_fact)
-        db.session.commit()
-    return redirect("/")
+# @app.route("/change_facts", methods=["POST"])
+# def change_facts():
+#     if request.method == "POST":
+#         change_facts = request.get_json()
+#         for key, value in change_facts.items():
+#             if Fact.query.filter(Fact.name == key).first() is None:
+#                 new_fact = Fact(name=key, value=value)
+#                 db.session.add(new_fact)
+#         db.session.commit()
+#     return redirect("/")
