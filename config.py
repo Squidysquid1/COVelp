8e8ab4a21f5889cf53fa90d9e81055bd094f02a5
StanleyN1 <stan.nicholson1@gmail.com>
2020-09-19 19:17:06 -0500

    fix cache??

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 DEBUG=True
-
+SEND_FILE_MAX_AGE_DEFAULT = 0
 base = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"# + os.path.join(base, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
